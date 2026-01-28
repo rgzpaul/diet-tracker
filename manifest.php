e2856e92e15379af5449434d6ba445db2e08deda
Paul Rodriguez <71314522+rgzpaul@users.noreply.github.com>
2026-01-28 16:59:08 +0100

    fix icon

--- manifest.php
+++ manifest.php
@@ -11,7 +11,7 @@ $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/') . '/';
   "theme_color": "#57534E",
   "icons": [
     {
-      "src": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect fill='%2357534E' width='100' height='100' rx='20'/><g transform='rotate(45 50 50)'><path d='M50 25v50M42 25v15c0 4 4 8 8 8s8-4 8-8V25' stroke='white' stroke-width='5' fill='none' stroke-linecap='round' stroke-linejoin='round'/></g></svg>",
+      "src": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect fill='%2357534E' width='100' height='100' rx='20' ry='20'/><path d='M68.3,31.7l-43.6,43.6M61.3,24.7l-13.1,13.1c-3.5,3.5-3.5,10.5,0,14s10.5,3.5,14,0l13.1-13.1' stroke='white' stroke-width='5' fill='none' stroke-linecap='round' stroke-linejoin='round'/></svg>",
       "sizes": "any",
       "type": "image/svg+xml",
       "purpose": "any"
